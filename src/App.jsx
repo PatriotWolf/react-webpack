6d97f04b9e3f8b74314ce5b49c154bd7c9a98feb
Mr.Worldwide <syahfis@gmail.com>
2023-12-06 19:53:14 +0800

    feature: Prettier Setup && code format && VScode editor setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-function App() {
+import React from 'react';
+const App = () => {
   return <h1>Hello World</h1>;
-}
+};
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,23 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const ESLintPlugin = require("eslint-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
 module.exports = {
   resolve: {
-    extensions: [".js", ".jsx"],
+    extensions: ['.js', '.jsx'],
   },
   module: {
     rules: [
       {
         test: /.(js)x?$/,
         exclude: /node_modules/,
-        use: { loader: "babel-loader" },
+        use: { loader: 'babel-loader' },
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./public/index.html",
-      filename: "./index.html",
+      template: './public/index.html',
+      filename: './index.html',
     }),
-    new ESLintPlugin({ extensions: ["js", "jsx", "ts", "tsx"] }),
+    new ESLintPlugin({ extensions: ['js', 'jsx', 'ts', 'tsx'] }),
   ],
 };
