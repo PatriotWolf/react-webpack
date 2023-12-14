ede06f12b9913d625eaf302439ba4e07a587d225
Mr.Worldwide <syahfis@gmail.com>
2023-12-14 11:37:59 +0800

    feature: testing setup && test example with CounterButton

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
-import { Counter } from './components/Counter';
-
+import { useState } from 'react';
+import CounterButton from './components/CounterButton';
 const App = () => {
+  const [count, setCount] = useState<number>(0);
+
   return (
     <div>
       <h1>Hello World</h1>
-      <Counter />
+      <h3>Component Counter Testing</h3>
+      <CounterButton
+        count={count}
+        setCount={() => setCount(count => count + 1)}
+      />
     </div>
   );
 };
